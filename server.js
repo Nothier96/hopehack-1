5ecab93dd83c0481ded69d8d4022b38c866ef56e
Thierno Sissoko <thiernosissoko@AdministorsMBP2.attlocal.net>
2022-06-30 15:25:26 -0400

    conversion

--- server.js
+++ server.js
@@ -1,20 +1,48 @@
-const express = require('express')
+const fetch = require("node-fetch");
+const express = require("express");
 const app = express();
-const port = process.env.PORT ||  3000;
+const port = process.env.PORT || 3000;
 
-const path = require('path');
+const path = require("path");
 
-app.use(express.static('public'));
-app.use(express.static(path.resolve(__dirname,'public')));
-app.get('/', (req, res) => {
-  res.sendFile('index.html',{root:__dirname});
+app.use(express.static("public"));
+app.use(express.static(path.resolve(__dirname, "public")));
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: __dirname });
 });
 
-app.get('/contact', (req, res) => {
-    res.sendFile('contact.html',{root:__dirname});
+app.get("/contact", (req, res) => {
+  res.sendFile("contact.html", { root: __dirname });
+});
+const client_id = " oobVGja6bsRkU5qU1PWjcMnjEK0nIMfgBttT0V8V";
+const client_secret =
+  "n8MLcxcTb4xS0cbKE4stYVWeawQ2u6oVxeHZDAD21PGFXfM8SqtD59PSHMomab9n233p5B0MrkLeGAjK9xMz7YqJoeQKrxRqHNxecbmyqvnPugnemfwQr4yKF0RmNw7L";
+const client = Buffer.from(`${client_id}:${client_secret}`).toString("base64");
+fetch("https://redventures.udemy.com/api-2.0/courses/", {
+  headers: {
+    Authorization: `Basic ${client}`,
+  },
+})
+  .then((res) => res.json())
+  .then((json) => console.log(json))
+  .catch((err) => {
+    console.log(err);
   });
-  
 
+// fetch("https://redventures.udemy.com/api-2.0/courses/", {
+//   headers: {
+//     Authorization:
+//       "Basic " +
+//       Buffer.from(
+//         "IG9vYlZHamE2YnNSa1U1cVUxUFdqY01uakVLMG5JTWZnQnR0VDBWOFY6bjhNTGN4Y1RiNHhTMGNiS0U0c3RZVldlYXdRMnU2b1Z4ZUhaREFEMjFQR0ZYZk04U3F0RDU5UFNITW9tYWI5bjIzM3A1QjBNcmtMZUdBaks5eE16N1lxSm9lUUtyeFJxSE54ZWNibXlxdm5QdWduZW1md1FyNHlLRjBSbU53N0w"
+//       ).toString("base64"),
+//   },
+// })
+//   .then((res) => res.json())
+//   .then((json) => console.log(json))
+//   .catch((err) => {
+//     console.log(err);
+//   });
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 });
